149f478e9248e6038621f7cf5c924b05ac4e029c
Cristian <71907925+Spek7ro@users.noreply.github.com>
2025-12-20 21:57:45 -0600

    doc: documentacion de un api

--- api/main.py
+++ api/main.py
@@ -2,18 +2,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# app.title = "Mi Primera API"
+# app.version = "0.1.0"
+
 # Iniciar el servidor uvicorn main:app --reload
 
 # Hola mundo con fast api
-@app.get("/") 
+@app.get("/", tags=['Home']) 
 async def root():
     return {"Hello": "World",
             "Hola": "Mundo", 
             "Bienvenido": "a FastAPI",
-            "Python": "es una mierda"
+            "Python": "Es un lenguaje de programación",
             }
 
-@app.get("/url") 
+@app.get("/url", tags=['Url']) 
 async def url():
     return {"url": "https://fastapi.tiangolo.com/"}
 
