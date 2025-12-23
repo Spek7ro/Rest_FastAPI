eb60c82bf4067bdf449412fe5b677072614dcded
Cristian <71907925+Spek7ro@users.noreply.github.com>
2025-12-22 21:43:04 -0600

    Modelos de datos: Validacion usando pydantic en los métodos

--- api/main.py
+++ api/main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional, List
 
 app = FastAPI()
 
 # app.title = "Mi Primera API"
 # app.version = "0.1.0"
 
+# Modelo de datos (Movie) para registrar y consultar
+class Movie(BaseModel):
+    id: int
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
+
+# Modelo de datos (MovieUpdate) para actualizar
+class MovieUpdate(BaseModel):
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
+
+# Lista de películas
 movies = [
     {
         "id": 1,
@@ -25,7 +45,6 @@ movies = [
     },
 ]
 
-
 # Hola mundo con fast api
 @app.get("/", tags=['Home']) 
 async def home():
@@ -33,61 +52,48 @@ async def home():
 
 # Podemos crear un endpoint que retorne un diccionario
 @app.get("/movies", tags=['Movies'])
-async def get_movies():
+async def get_movies() -> List[Movie]:
     return movies
-
 
 # Parametros de ruta
 @app.get("/movies/{id}", tags=['Movies'])
-async def get_movie_id(id: int):
+async def get_movie_id(id: int) -> Movie:
     movie = next(filter(lambda movie: movie["id"] == id, movies), None)
     if movie:
         return movie
     else:
         return {"error": "Movie not found"}
 
-
 # Parámetros de query
 @app.get("/movies/", tags=['Movies'])
-async def get_movie_by_category(category: str):
+async def get_movie_by_category(category: str) -> Movie:
     movies_by_category = list(filter(lambda movie: movie["category"] == category, movies))
     return movies_by_category
 
 # Método POST (Request Body)
 @app.post("/movies", tags=['Movies'])
-async def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), 
-                       year: int = Body(), rating: float = Body(), category: str = Body()):
-    new_movie = {
-        "id": id,
-        "title": title,
-        "overview": overview,
-        "year": year,
-        "rating": rating,
-        "category": category
-    }
-    movies.append(new_movie)
-    return new_movie
-
+async def create_movie(movie: Movie) -> List[Movie]:
+    # movies.append(movie.dict()) # dict() esta deprecated
+    movies.append(movie.model_dump()) 
+    return movies
 
 # Método PUT (Actualizar por id)
 @app.put("/movies/{id}", tags=['Movies'])
-async def update_movie(id: int, title: str = Body(), overview: str = Body(),
-                       year: int = Body(), rating: float = Body(), category: str = Body()):
-    movie = next(filter(lambda movie: movie["id"] == id, movies), None)
-    if movie:
-        movie["title"] = title
-        movie["overview"] = overview
-        movie["year"] = year
-        movie["rating"] = rating
-        movie["category"] = category
+async def update_movie(id: int, movie: MovieUpdate) -> List[Movie]:
+    movie_to_update = next(filter(lambda movie: movie["id"] == id, movies), None)
+    if movie_to_update:
+        movie_to_update["title"] = movie.title
+        movie_to_update["overview"] = movie.overview
+        movie_to_update["year"] = movie.year
+        movie_to_update["rating"] = movie.rating
+        movie_to_update["category"] = movie.category
         return movies
     else:
         return {"error": "Movie not found"}
 
-
 # Método DELETE (Eliminar por id)
 @app.delete("/movies/{id}", tags=['Movies'])
-async def delete_movie(id: int):
+async def delete_movie(id: int) -> List[Movie]:
     movie = next(filter(lambda movie: movie["id"] == id, movies), None)
     if movie:
         movies.remove(movie)
@@ -101,9 +107,3 @@ async def delete_movie(id: int):
 #     return HTMLResponse("<h1>Hola mundo con fast api</h1>"
 #                         "<h3>Textoooooooo</h3>"
 #                         )
-
-@app.get("/url", tags=['Url']) 
-async def url():
-    return {"url": "https://fastapi.tiangolo.com/"}
-
-
